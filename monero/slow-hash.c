77891961c4672ebb0c73e4991b738b1b0d44463f
Shang <36684350+shangproject@users.noreply.github.com>
2018-04-18 16:22:22 +0800

    Update slow-hash.c

--- monero/slow-hash.c
+++ monero/slow-hash.c
@@ -39,8 +39,8 @@
 #include "hash-ops.h"
 #include "oaes_lib.h"
 
-#define MEMORY         (1 << 21) // 2MB scratchpad
-#define ITER           (1 << 20)
+#define MEMORY         (1 << 22) // 2MB scratchpad
+#define ITER           (1 << 21)
 #define AES_BLOCK_SIZE  16
 #define AES_KEY_SIZE    32
 #define INIT_SIZE_BLK   8
